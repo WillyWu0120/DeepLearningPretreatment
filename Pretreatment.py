9ab97732e73d83a25855cbba5d498aec789e6ad3
wancat <wancat@wancat.cc>
2021-12-17 10:21:52 +0800

    Rewrite as Tk class

--- Pretreatment.py
+++ Pretreatment.py
@@ -7,100 +7,100 @@ from os import listdir
 from os.path import isfile, join
 import json
 import numpy as np
-result = []
-def SelectFile():
-    return filedialog.askopenfilename(
-        initialdir='./Pictures', title="select file", 
-        filetypes = (("png files","*.png"),("all files","*.*"))).split('/')[-1]
 
-def Gui():
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.result = []
+        self.geometry('250x500')
+        self.pitch = tk.IntVar()
+        noteList = ["Re", "Do", "Si", "La", "So", "Fa", "Mi", "Re", "Do", "Si", "La", "So", "Fa", "Mi", "Re", "Do", "Si", "La", "So"]
+        for i in range(9, -10,-1):
+            button = tk.ttk.Radiobutton(self, text=noteList[-(i-9)], variable=self.pitch, value=i)
+            button.grid(column=0, row=-(i-9))
+        
+        durations = [4,2,1,0.5,0.25,0.125]
+        self.duration = tk.IntVar()
+        for i in durations:
+            button = tk.ttk.Radiobutton(self, text=i, variable=self.duration, value=i)
+            button.grid(column=1, row=durations.index(i))
+            
+        self.dots = tk.IntVar()
+        for i in range(0,3):
+            button = tk.ttk.Radiobutton(self, text=i, variable=self.dots, value=i)
+            button.grid(column=2, row=i)
+            
+        self.accidental = tk.IntVar()
+        accidental_labels = ['bb', 'b', '', '#', '##']
+        for i in range(2,-3 ,-1):
+            button = tk.ttk.Radiobutton(self, text=accidental_labels[i+2], variable=self.accidental, value=i)
+            button.grid(column=3, row=-(i-2))
+        
+        doneButton = tk.Button(self, text="Done", width=8,
+                                command=self.done)
+        doneButton.grid(column=0, row=19)
+        selectButton = tk.Button(self, text="select", width=8, command=self.select)
+        selectButton.grid(column=1, row=19)
+        self.load()
     
-    def Done():
-        result.append({
-            "id": len(result)+1,
-            "pitch": pitch.get(),
-            "duration": duration.get(),
-            "dots": dots.get(),
-            "accidental" : accidental.get()
+    def load(self):
+        picName = filedialog.askopenfilename(
+            initialdir='./Pictures', title="select file", 
+            filetypes = (("png files","*.png"),("all files","*.*"))).split('/')[-1]
+
+        self.picName = picName
+        src = cv2.imread('./Pictures/' + str(picName), cv2.IMREAD_COLOR)
+        self.src = src
+        nr,nc = src.shape[:2]
+        self.image = cv2.resize(src, (int(nc*(700/nr)), 700), interpolation=cv2.INTER_AREA)
+        self.dir = os.path.join('Labels', picName)
+        self.path = os.path.join(self.dir, 'data.json')
+        Path("./Labels/").mkdir(exist_ok=True)
+        Path("./Labels/"+ str(picName)).mkdir(exist_ok=True)
+        if (os.path.isfile("./Labels/" + str(picName) + "/" + "data.json")):
+            with open("./Labels/" + str(picName) + "/" + "data.json", 'r') as data:
+                if os.stat("./Labels/" + str(picName) + "/" + "data.json").st_size > 0:
+                    self.result = json.load(data)        
+        else:
+            open("./Labels/" + str(picName) + "/" + "data.json", 'w+')
+    
+
+    def done(self):
+        self.result.append({
+            "id": len(self.result)+1,
+            "pitch": self.pitch.get(),
+            "duration": self.duration.get(),
+            "dots": self.dots.get(),
+            "accidental" : self.accidental.get()
         })
-        app.destroy()
-    
-    app = tk.Tk() 
-    app.geometry('250x500')
-    pitch = tk.IntVar()
-    noteList = ["Re", "Do", "Si", "La", "So", "Fa", "Mi", "Re", "Do", "Si", "La", "So", "Fa", "Mi", "Re", "Do", "Si", "La", "So"]
-    for i in range(9, -10,-1):
-        button = tk.ttk.Radiobutton(app, text=noteList[-(i-9)], variable=pitch, value=i)
-        button.grid(column=0, row=-(i-9))
-    
-    durations = [4,2,1,0.5,0.25,0.125]
-    duration = tk.IntVar()
-    for i in durations:
-        button = tk.ttk.Radiobutton(app, text=i, variable=duration, value=i)
-        button.grid(column=1, row=durations.index(i))
-        
-    dots = tk.IntVar()
-    for i in range(0,3):
-        button = tk.ttk.Radiobutton(app, text=i, variable=dots, value=i)
-        button.grid(column=2, row=i)
-        
-    accidental = tk.IntVar()
-    accidental_labels = ['bb', 'b', '', '#', '##']
-    for i in range(2,-3 ,-1):
-        button = tk.ttk.Radiobutton(app, text=accidental_labels[i+2], variable=accidental, value=i)
-        button.grid(column=3, row=-(i-2))
-    
-    doneButton = tk.Button(app, text="Done", width=8,
-                              command=Done)
-    doneButton.grid(column=0, row=19)
-    
-    app.mainloop()
+        self.save_note()
+        with open(self.path, 'w') as outfile:
+            json.dump(self.result, outfile)
+        cv2.destroyAllWindows()
 
-def SaveNote(src, bbox):
-    nr,nc = src.shape[:2]
-    note = src[int(bbox[1]*nr/700):int((bbox[1]+bbox[3])*nr/700), int(bbox[0]*nr/700):int((bbox[0]+bbox[2])*nr/700)]
-    note = cv2.cvtColor(note, cv2.COLOR_BGR2GRAY)
-    noteR,noteC = note.shape[:2]
-    if noteR > noteC*2 :
-        new_image_height = noteR
-        new_image_width = noteR // 2
-    else :
-        new_image_height = noteC * 2
-        new_image_width = noteC
-    resultImg = np.full((new_image_height,new_image_width), 255, dtype=np.uint8)
-    y_center = (new_image_height - noteR) // 2
-    x_center = (new_image_width - noteC) // 2
-    resultImg[y_center:y_center+noteR, x_center:x_center+noteC] = note
-    
-    cv2.imwrite("./Labels/"+str(picName)+"/"+str(len(result))+".png", resultImg)
+    def select(self):        
+        self.bbox = cv2.selectROI(self.image, False)
+
+    def save_note(self):
+        bbox = self.bbox
+        nr,nc = self.src.shape[:2]
+        note = self.src[int(bbox[1]*nr/700):int((bbox[1]+bbox[3])*nr/700), int(bbox[0]*nr/700):int((bbox[0]+bbox[2])*nr/700)]
+        note = cv2.cvtColor(note, cv2.COLOR_BGR2GRAY)
+        noteR,noteC = note.shape[:2]
+        if noteR > noteC*2 :
+            new_image_height = noteR
+            new_image_width = noteR // 2
+        else :
+            new_image_height = noteC * 2
+            new_image_width = noteC
+        resultImg = np.full((new_image_height,new_image_width), 255, dtype=np.uint8)
+        y_center = (new_image_height - noteR) // 2
+        x_center = (new_image_width - noteC) // 2
+        resultImg[y_center:y_center+noteR, x_center:x_center+noteC] = note
+        
+        cv2.imwrite(f"./Labels/{self.picName}/{len(self.result)}.png", resultImg)
 
 if __name__ == '__main__' :
-    picName = SelectFile()
-    src = cv2.imread('./Pictures/' + str(picName), cv2.IMREAD_COLOR)
-    nr,nc = src.shape[:2]
-    image = cv2.resize(src, (int(nc*(700/nr)), 700), interpolation=cv2.INTER_AREA)
-    Path("./Labels/").mkdir(exist_ok=True)
-    Path("./Labels/"+ str(picName)).mkdir(exist_ok=True)
-    if (os.path.isfile("./Labels/" + str(picName) + "/" + "data.json")):
-        with open("./Labels/" + str(picName) + "/" + "data.json", 'r') as data:
-            if os.stat("./Labels/" + str(picName) + "/" + "data.json").st_size > 0:
-                result = json.load(data)        
-    else:
-        open("./Labels/" + str(picName) + "/" + "data.json", 'w+')
-    
-    while True:
-        bbox = cv2.selectROI(image, False)
-        if not bbox:
-            break
-    
-        Gui()
-        SaveNote(src,bbox)
-        with open("./Labels/" + str(picName) + "/" + "data.json", 'w') as outfile:
-            json.dump(result, outfile)
-        
-        k = cv2.waitKey(0) & 0xff
-        
-        if k == ord('q') : 
-            break
-    
-    cv2.destroyAllWindows()
+    app = App()
+    app.mainloop()
+ 
