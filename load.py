3ce778466efd0267651f499da303572ddb2e1aeb
wancat <wancat@wancat.cc>
2021-12-24 12:11:29 +0800

    resize, save to npz

--- load.py
+++ load.py
@@ -2,6 +2,10 @@ import json
 import glob
 from os import path
 import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
+
+SHAPE = (50, 100)
 
 
 def folder_list():
@@ -18,30 +22,51 @@ def load_labels(folder):
 
 
 def load_image(image_path, label_list):
-    image = plt.imread(image_path)
+    image = Image.open(image_path)
+    image = image.resize(SHAPE)
     image_id = path.splitext(path.basename(image_path))[0]
     label = [v for v in label_list if v["id"] == int(image_id)][0]
     print(image_id, label)
-    return image, label
+    return np.array(image), label
 
 
 def load():
     folders = folder_list()
-    data = []
-    labels = []
+    images = []
+    pitch = []
+    duration = []
+    dots = []
+    accidental = []
     for folder in folders:
         print(folder)
 
         l = load_labels(folder)
         for image_path in image_list(folder):
             image, label = load_image(image_path, l)
-            data.append(image)
-            labels.append(label)
+            print(image.shape)
+            images.append(image)
+            pitch.append(label['pitch'])
+            duration.append(label['duration'])
+            dots.append(label['dots'])
+            accidental.append(label['accidental'])
 
         print('-' * 50)
 
-    print(len(data), len(labels))
+    print(len(images), len(pitch))
+    return {
+        "images": np.array(images),
+        "pitch": np.array(pitch, dtype=np.int32),
+        "duration": np.array(duration, dtype=np.float32),
+        "dots": np.array(dots, dtype=np.uint8),
+        "accidental": np.array(accidental, dtype=np.uint8),
+    }
+
+
+def save(data):
+    np.savez_compressed('data.npz', **data)
 
 
 if __name__ == '__main__':
-    load()
+    data = load()
+    print(data['images'].shape)
+    save(data)
